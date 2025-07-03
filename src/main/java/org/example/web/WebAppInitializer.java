ecce2766781dc335deac45a9b2ad3a6d4f9db91d
Guillaume HUSTA <guillaume.husta@gmail.com>
2025-07-04 00:08:38 +0200

    feat: Bind Spring's root application context to servlet container lifecycle

--- src/main/java/org/example/web/WebAppInitializer.java
+++ src/main/java/org/example/web/WebAppInitializer.java
@@ -1,23 +1,30 @@
 package org.example.web;
 
-import org.example.config.AppConfig;
-import org.springframework.web.WebApplicationInitializer;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
-import org.springframework.web.servlet.DispatcherServlet;
-
 import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRegistration;
+import org.example.config.AppConfig;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
 public class WebAppInitializer implements WebApplicationInitializer {
 
     @Override
     public void onStartup(ServletContext servletContext) throws ServletException {
-        AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
-        context.register(AppConfig.class);
+        // Create the 'root' Spring application context
+        AnnotationConfigWebApplicationContext rootContext = new AnnotationConfigWebApplicationContext();
+        rootContext.register(AppConfig.class);
+
+        // ⚠️ Manage the lifecycle of the root application context
+        // If exceptions, prevent application to be deployed in container
+        servletContext.addListener(new ContextLoaderListener(rootContext));
+
+        // ℹ️ Optional: Create context for Spring MVC, and pass it to DispatcherServlet
 
         // Create DispatcherServlet
-        DispatcherServlet dispatcherServlet = new DispatcherServlet(context);
+        DispatcherServlet dispatcherServlet = new DispatcherServlet(rootContext);
         ServletRegistration.Dynamic registration = servletContext.addServlet("dispatcher", dispatcherServlet);
         registration.setLoadOnStartup(1);
         registration.addMapping("/");
