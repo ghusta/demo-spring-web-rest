6844931cbd724d326e4765c13bf03c69ba6c40df
Guillaume HUSTA <guillaume.husta@gmail.com>
2025-07-11 14:45:30 +0200

    docs: Add javadoc in WebAppInitializer

--- src/main/java/org/example/web/WebAppInitializer.java
+++ src/main/java/org/example/web/WebAppInitializer.java
@@ -11,6 +11,9 @@ import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
 
+/**
+ * @see org.springframework.web.SpringServletContainerInitializer
+ */
 public class WebAppInitializer implements WebApplicationInitializer {
 
     @Override
