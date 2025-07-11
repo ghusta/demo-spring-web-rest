72197f15e1b333629a3bfa8691203a7cc0084f5b
Guillaume HUSTA <guillaume.husta@gmail.com>
2025-07-11 15:13:46 +0200

    feat: Bind Spring's root application context to servlet container lifecycle

--- src/main/java/org/example/web/WebAppInitializer.java
+++ src/main/java/org/example/web/WebAppInitializer.java
@@ -2,6 +2,7 @@ package org.example.web;
 
 import org.example.config.AppConfig;
 import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
@@ -13,12 +14,20 @@ public class WebAppInitializer implements WebApplicationInitializer {
 
     @Override
     public void onStartup(ServletContext servletContext) throws ServletException {
-        AnnotationConfigWebApplicationContext appContext = new AnnotationConfigWebApplicationContext();
-        appContext.register(AppConfig.class);
+        // Create the 'root' Spring application context
+        AnnotationConfigWebApplicationContext rootContext = new AnnotationConfigWebApplicationContext();
+        rootContext.register(AppConfig.class);
+
+        // ⚠️ Manage the lifecycle of the root application context
+        // If exceptions, prevent application to be deployed in container
+        servletContext.addListener(new ContextLoaderListener(rootContext));
+
+        // ℹ️ Optional: Create context for Spring MVC, and pass it to DispatcherServlet
 
         // Create DispatcherServlet
-        ServletRegistration.Dynamic dispatcher = servletContext.addServlet("dispatcher", new DispatcherServlet(appContext));
-        dispatcher.setLoadOnStartup(1);
-        dispatcher.addMapping("/");
+        DispatcherServlet dispatcherServlet = new DispatcherServlet(rootContext);
+        ServletRegistration.Dynamic registration = servletContext.addServlet("dispatcher", dispatcherServlet);
+        registration.setLoadOnStartup(1);
+        registration.addMapping("/");
     }
 }
